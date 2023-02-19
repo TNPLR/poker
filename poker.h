c5f236fca344af06aef67dbc793b13e0be190cea
C.H. Hsiao <hsiaosvideo@gmail.com>
2023-02-19 12:47:50 +0800

    Edit some features

--- poker.h
+++ poker.h
@@ -1,4 +1,4 @@
-/* poker.h Copyright 2019 TNPLR
+/* poker.h Copyright 2019, 2023 TNPLR
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -39,18 +39,6 @@ enum {
 	SPADE, /**< SPADE*/
 };
 
-#if unix || __GNUC__
-/**
- * Graph of suits (UTF-8)
- */
-constexpr const char *suit_image[4] = {"\xe2\x99\xa3", "\xe2\x99\xa6", "\xe2\x99\xa5", "\xe2\x99\xa0"};
-#elif __WIN32 || __WINNT
-/**
- * Graph of suits (Code Page 437)
- */
-constexpr const char *suit_image[4] = {"\x5", "\x4", "\x3", "\x6"};
-#endif // unix
-
 /**
  * A poker card
  */
@@ -84,27 +72,46 @@ struct card {
 	 * @param cd The card to output
 	 */
 	friend std::ostream& operator<<(std::ostream& os, const card& cd);
+	/** Get the suit of card (string) */
+	char const* suit_sign() const;
+	/** Get the rank of card (string) */
+	char const* card_rank() const;
+	bool operator==(card const& rop)
+	{
+		return number == rop.number && suit == rop.suit;
+	}
+#if unix || __GNUC__
+	/**
+	 * Graph of suits (UTF-8)
+	 */
+	static constexpr const char *const suit_image[4]{"\xe2\x99\xa3", "\xe2\x99\xa6", "\xe2\x99\xa5", "\xe2\x99\xa0"};
+#elif __WIN32 || __WINNT
+	/**
+	 * Graph of suits (Code Page 437)
+	 */
+	static constexpr const char *const suit_image[4]{"\x5", "\x4", "\x3", "\x6"};
+#endif // unix
+	static constexpr char const * const cardname[15]{"", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "JOKER"};
 };
+
+char const* card::suit_sign() const
+{
+	return suit_image[suit];
+}
+
+char const* card::card_rank() const
+{
+	return cardname[number];
+}
+
 
 class deck {
 public:
-	/** @return The vector of this deck of cards*/
-	std::vector<struct card>& get_vec(void)
-	{
-		return pile;
-	}
-
-	/** @return The const vector of this deck of cards*/
-	const std::vector<struct card>& get_vec(void) const
-	{
-		return pile;
-	}
-
-	/** @return The const vector of this deck of cards*/
-	const std::vector<struct card>& get_vec_const(void) const
-	{
-		return pile;
-	}
+	/** Remove the card which is identical to c from the deck
+	 * @param c Card to remove
+	 * @return The card that is removed (i.e. c)
+	*/
+	card remove(card const& c);
 
 	/** Put a card into the deck
 	 * @param c Card to put into
@@ -160,8 +167,61 @@ public:
 	{
 		return pile[index];
 	}
+
+	/** This enumerator is the mode of sorting */
+	enum class sort_mode {
+		rank_first_descending, /**< Sort in SA HA DA CA SK HK ... D2 C2 order */
+		suit_first_descending, /**< Sort in SA SK ... S2 HA HK ... C3 C2 order */
+	};
+
+	/** Sort the deck
+	 * @param mode the mode of sorting
+	 */
+	void sort(sort_mode mode = sort_mode::rank_first_descending);
+
+	/** This enum is the mode of printing a deck */
+	enum class print_mode {
+		no_sort, /**< Print without additional sorting */
+		sort_by_number, /**< Sort in ascending order from A 2 3 ...*/
+		sort_by_suit, /**< Sort by suit first, and then the number*/
+		rank_only, /**< print only the rank */
+	};
+
+
+	/** Set the Printing mode
+	 * @param mode The mode of printing
+	 */
+	inline void set_print_mode(print_mode mode = print_mode::no_sort) const
+	{
+		switch (mode) {
+			case print_mode::sort_by_number:
+				output_stream = &deck::sort_by_number_ostream;
+				break;
+			case print_mode::sort_by_suit:
+				output_stream = &deck::sort_by_suit_ostream;
+				break;
+			case print_mode::no_sort:
+				output_stream = &deck::no_sort_ostream;
+				break;
+			case print_mode::rank_only:
+				output_stream = &deck::rank_only_ostream;
+				break;
+		}
+	}
+
+	/** Get the subset of deck of a specific suit
+	 * @param suit the suit (enumerator)
+	 */
+	deck suit_subdeck(int suit) const;
 private:
-	std::vector<struct card> pile;
+	mutable std::ostream& (deck::*output_stream)(std::ostream &os) const = &deck::no_sort_ostream;
+
+	std::vector<struct card> pile; /**< Internal storage of cards */
+
+	std::ostream& no_sort_ostream(std::ostream &os) const;
+	std::ostream& sort_by_number_ostream(std::ostream &os) const;
+	std::ostream& rank_only_ostream(std::ostream &os) const;
+	std::ostream& sort_by_suit_ostream(std::ostream &os) const;
 };
 
 class poker {
@@ -187,9 +247,9 @@ public:
 
 	/** Play a card from the player to the card pile.
 	 * @param player_no The number of the player.
-	 * @param card_no The card number in the player's deck
+	 * @param card The card in the player's deck
 	 */
-	void play(unsigned int player_no, unsigned int card_no);
+	void play(unsigned int player_no, card const& c);
 
 	/** Deal some card to each person.
 	 * @param card_per_person How many card should the function deal to each player.
@@ -212,26 +272,9 @@ public:
 		return player_card[player_no];
 	}
 
-	/** Sort the cards of every player. */
-	void sort_player_card()
-	{
-		sort_player_card([](struct card a, struct card b)->bool {
-			if (a.number < b.number) {
-				return true;
-			} else if (a.number == b.number) {
-				if (a.suit < b.suit) {
-					return true;
-				}
-			}
-			return false;
-		});
-	}
+	/** Sort the cards of every player in rank-first ascending order*/
+	void sort_player_card();
 
-	/** Sort the cards of every player. An internal Function
-	 * @param f Compare 2 cards
-	 */
-	template <typename T>
-	void sort_player_card(T f);
 	~poker() = default;
 
 private:
@@ -242,12 +285,14 @@ private:
 
 std::ostream& operator<<(std::ostream& os, const card& cd)
 {
-	if (cd.number != 14) {
-		os << suit_image[cd.suit] << ' ' << std::setw(2) << static_cast<unsigned>(cd.number);
-	} else {
-		os << "JOKER";
-	}
-	return os;
+	switch (cd.number) {
+		case 14:
+			os << cd.card_rank();
+			return os;
+		default:
+			os << cd.suit_sign() << ' ' << std::setw(2) << cd.card_rank();
+			return os;
+	};
 }
 
 std::ostream& operator<<(std::ostream& os, poker pk)
@@ -258,22 +303,99 @@ std::ostream& operator<<(std::ostream& os, poker pk)
 
 std::ostream& operator<<(std::ostream &os, const deck& dk)
 {
-	if (dk.empty()) {
+	return (dk.*dk.output_stream)(os);
+}
+
+std::ostream& deck::no_sort_ostream(std::ostream &os) const
+{
+	if (empty()) {
 		return os;
 	}
-	os << dk[0];
-	for (unsigned int i = 1; i < dk.size(); ++i) {
-		os << "  " << dk[i];
+	os << (*this)[0];
+	for (unsigned int i = 1; i < size(); ++i) {
+		os << "  " << (*this)[i];
 	}
 	return os;
 }
 
-template <typename T>
-void poker::sort_player_card(T f)
+card deck::remove(card const& c)
 {
-	for (auto &vec : player_card) {
-		std::sort(vec.get_vec().begin(), vec.get_vec().end(), f);
+	std::vector<card>::iterator it = std::find(pile.begin(), pile.end(), c);
+	if (it == pile.end()) {
+		throw std::invalid_argument("He or she does not have the card");
 	}
+	pile.erase(it);
+	return c;
+}
+
+std::ostream& deck::sort_by_number_ostream(std::ostream &os) const
+{
+	deck tmp = *this;
+	tmp.sort();
+	os << tmp;
+	return os;
+}
+
+std::ostream& deck::rank_only_ostream(std::ostream &os) const
+{
+	std::for_each(pile.cbegin(), pile.cend(), [&os](card const& crd) -> void {
+			os << crd.card_rank() << ' ';
+	});
+	return os;
+}
+
+
+std::ostream& deck::sort_by_suit_ostream(std::ostream &os) const
+{
+	for (int i = SPADE; i >= CLUB; --i) {
+		deck tmp = suit_subdeck(i);
+		tmp.set_print_mode(deck::print_mode::rank_only);
+		os << card::suit_image[i] << "  " << tmp << "\n";
+	}
+	return os;
+}
+
+deck deck::suit_subdeck(int suit) const
+{
+	deck dktmp;
+	std::for_each(pile.cbegin(), pile.cend(), [=, &dktmp](card const& crd) -> void {
+			if (crd.suit == suit) {
+				dktmp.push_back(crd);
+			}
+	});
+	return dktmp;
+}
+
+void deck::sort(sort_mode mode)
+{
+	switch (mode)
+	{
+	case sort_mode::rank_first_descending:
+		std::sort(pile.begin(), pile.end(), [](card const &a, card const &b)->bool {
+			if (a.number != b.number) {
+				return a.number == 1 || (b.number != 1 && a.number > b.number);
+			}
+			return a.suit > b.suit;
+		});
+		return;
+	case sort_mode::suit_first_descending:
+		std::sort(pile.begin(), pile.end(), [](card const &a, card const &b)->bool {
+			if (a.suit == b.suit) {
+				return a.number == 1 || (b.number != 1 && a.number > b.number);
+			}
+			return a.suit > b.suit;
+		});
+		break;
+	default:
+		return;
+	}
+}
+
+void poker::sort_player_card()
+{
+	std::for_each(player_card.begin(), player_card.end(), [](deck& tmp) {
+		tmp.sort();
+	});
 }
 
 poker::poker(unsigned int deck, unsigned int player, bool joker) : players{player}
@@ -323,6 +445,14 @@ void poker::shuffle(unsigned int time)
 	}
 }
 
+void poker::play(unsigned int player_no, card const& c)
+{
+	if (player_no >= players) {
+		throw std::out_of_range("The player number is too large");
+	}
+	player_card[player_no].remove(c);
+}
+
 struct card poker::draw(unsigned int player_no)
 {
 	if (player_no >= players) {
@@ -331,12 +461,6 @@ struct card poker::draw(unsigned int player_no)
 	player_card[player_no].push_back(pile.back());
 	pile.pop_back();
 	return player_card[player_no].back();
-}
-
-void poker::play(unsigned int player_no, unsigned int card_no)
-{
-	pile.push_back((*this)[player_no][card_no]);
-	(*this)[player_no].get_vec().erase((*this)[player_no].get_vec().begin() + card_no);
 }
 
 void poker::deal(unsigned int card_per_person)
